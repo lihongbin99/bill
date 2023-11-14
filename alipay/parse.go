56af51dc16418cc38a8dc0d1ef8f04689c801447
LiHongBin <1197723607@qq.com>
2023-11-14 14:02:35 +0800

    修改了阿里云的规则

--- alipay/parse.go
+++ alipay/parse.go
@@ -33,36 +33,20 @@ func Parse(csvPath string) ([]model.Bill, error) {
 			return nil, err
 		}
 
-		start := false
 		for i := len(records) - 1; i >= 0; i-- {
 			record := records[i]
 			if len(record) < 1 {
 				continue
 			}
-
-			// 判断那里开始
-			if !start {
-				if strings.TrimSpace(record[0]) == "------------------------------------------------------------------------------------" {
-					start = true
-					continue
-				}
-			}
-
-			// 没开始则跳过
-			if !start {
-				continue
-			}
-
-			// 判断哪里结束
-			if strings.TrimSpace(record[0]) == "收/支" {
+			if record[0] == "交易时间" {
 				break
 			}
 
-			if len(record) != 12 {
+			if len(record) != 13 {
 				return nil, fmt.Errorf("wechat bill len error(%d): %s", len(record), record)
 			}
 
-			transactionHour, err := time.Parse("2006-01-02 15:04:05", strings.TrimSpace(record[10]))
+			transactionHour, err := time.Parse("2006-01-02 15:04:05", strings.TrimSpace(record[0]))
 			if err != nil {
 				return nil, err
 			}
@@ -70,17 +54,17 @@ func Parse(csvPath string) ([]model.Bill, error) {
 			bill := model.Bill{
 				App:                    "AliPay",
 				TransactionHour:        transactionHour,
-				TransactionType:        strings.TrimSpace(record[7]),
-				Counterparty:           strings.TrimSpace(record[1]),
-				OtherAccounts:          strings.TrimSpace(record[2]),
-				Commodity:              strings.TrimSpace(record[3]),
-				IncomeExpenditure:      strings.TrimSpace(record[0]),
-				Amount:                 strings.TrimSpace(record[5]),
-				PaymentMethod:          strings.TrimSpace(record[4]),
-				CurrentState:           strings.TrimSpace(record[6]),
-				TransactionNumber:      strings.TrimSpace(record[8]),
-				MerchantTrackingNumber: strings.TrimSpace(record[9]),
-				Remark:                 "",
+				TransactionType:        strings.TrimSpace(record[1]),
+				Counterparty:           strings.TrimSpace(record[2]),
+				OtherAccounts:          strings.TrimSpace(record[3]),
+				Commodity:              strings.TrimSpace(record[4]),
+				IncomeExpenditure:      strings.TrimSpace(record[5]),
+				Amount:                 strings.TrimSpace(record[6]),
+				PaymentMethod:          strings.TrimSpace(record[7]),
+				CurrentState:           strings.TrimSpace(record[8]),
+				TransactionNumber:      strings.TrimSpace(record[9]),
+				MerchantTrackingNumber: strings.TrimSpace(record[10]),
+				Remark:                 strings.TrimSpace(record[11]),
 			}
 			billList = append(billList, bill)
 		}
